ff6152f84a804506e18a692403beb76dcae04f50
Jahanvi215 <tharu.jahanvi215@gmail.com>
2024-12-18 02:21:11 +0530

    bitchystory responsive

--- src/components/bitchyStory/BitchyStory.jsx
+++ src/components/bitchyStory/BitchyStory.jsx
@@ -21,7 +21,7 @@ const BitchyStory = () => {
     <img src={Bottom} alt="" />
         </div>
 
-        <div className="text-[#EBE235] font-primaryRegular text-center md:text-3xl uppercase text-[1rem] font-bold mt-2 bg-[#39C6CB] p-5 pb-10" 
+        <div className="text-[#EBE235] font-primaryRegular text-center md:text-2xl uppercase text-[1rem] font-bold mt-2 bg-[#39C6CB] p-5 pb-10" 
       style={{ 
         textShadow: "4px 4px 18px rgba(255, 255, 255, 0.6)",  // White glow
         WebkitTextStroke: "1px #F39734"  // White outline outside text
@@ -33,50 +33,50 @@ const BitchyStory = () => {
         <div className="bg-img">
         <img src={BitchyBg} alt="" />
     </div>
-    <div className="absolute top-0 right-0" >
-        <img src={Logo} alt="" className=' w-[60%]' style={{marginTop: "30%", }}/>
+    <div className="absolute top-0 md:mt-20 md:ml-[70%] " >
+        <img src={Logo} alt="" className=' w-[80%]'/>
     </div>
-    <div className="absolute bottom-0 left-0" style={{marginBottom: "18%", marginLeft:"16%"}}>
-        <img src={BitchyInsect} alt="" className='w-[30%]'/>
+    <div className="absolute z-50 bottom-0 left-0 md:mb-32 md:ml-20 lg:mb-44 lg:ml-40">
+        <img src={BitchyInsect} alt="" className='w-[30%] md:w-[25%]'/>
     </div>
     <div className="absolute bottom-0 left-0 z-10" style={{marginBottom: "23%", marginLeft:"27%"}}>
         <Cliva/>
     </div>
     <div>
-        <img src={BlueShape} alt="" className=" w-[60%] absolute top-0 " style={{marginTop:"12%"}}/>
-        <div className=" absolute top-0 text-white font-secondRegular text-xl"  style={{marginTop:"15%", marginLeft:"4%", lineHeight:"2.5"}}> 
-            <p>...Until she flew deeper into the forest. Shattered by the</p>
-            <p>view of disparity, exclusion, greed, and corruption. so on.</p>
-            <p>Selfishness reigned, and flowers drooped from the weight </p>
-            <p>of indifference. </p>
+        <img src={BlueShape} alt="" className=" w-[60%] absolute top-0 md:mt-32 lg:mt-44"/>
+        <div className="text-wrap text-clip w-[45%] absolute top-0 text-white font-secondRegular md:mt-36 md:ml-2 lg:mt-52 lg:ml-6 lg:text-xl lg:leading-9 leading-8 " > 
+       ...Until she flew deeper into the forest. Shattered by the
+            view of disparity, exclusion, greed, and corruption. so on.
+            Selfishness reigned, and flowers drooped  
+           from the weight of indifference. 
             </div>   
     </div>
 
     <div>
-        <img src={YellowShape} alt="" className=" w-[70%] h-[40%] absolute top-0 right-0" style={{marginTop:"18%"}}/>  
-       <div className=" absolute top-0 right-0  font-secondRegular text-right"  style={{marginTop:"20%", marginRight:"4%",  }}> 
-        <div className=' font-primaryRegular uppercase text-white text-xl' style={{WebkitTextStroke: "1px #228EFD", lineHeight:"2"}} >
-        <p >NO LONGER SHE WILL BE A PASSIVE  </p>
-        <p >OBSERVER </p>
+        <img src={YellowShape} alt="" className=" w-[70%] h-[40%] absolute top-0 right-0 md:mt-44 lg:mt-56" />  
+       <div className=" absolute top-0 right-0  font-secondRegular text-right md:mt-48 lg:mt-64 lg:mr-10 lg:leading-8"  > 
+        <div className=' font-primaryRegular uppercase text-white' style={{WebkitTextStroke: "1px #228EFD"}} >
+        <p >NO LONGER SHE WILL   </p>
+        <p >BE A PASSIVE OBSERVER </p>
         </div>
         
-        <div className='text-[#FF0A0A] text-xl ' style={{lineHeight:"2"}}>
-            <p>Cliva has to become bitchy Cliva to guide the promised gains to the</p>
-            <p>world from decentralisation tech. True change could come without a</p>
-            <p>system, without a structure where all voices could be heard. So, Cliva</p>
-            <p>set to work to build a network, a governance, one where the </p>
-            <p>suffering, the poor, and the innocent would have a place. Where no</p>
-            <p>one would be ignored. Where greed would be replaced by generosity</p>
-            <p>where cruelty would give way to compassion</p>
+        <div className='text-[#FF0A0A] text-wrap w-[110%] md:text-sm absolute right-2 lg: leading-9 lg:mt-5' >
+            Cliva has to become bitchy Cliva to guide the promised gains to the
+            world from decentralisation tech. True change could come without a
+            system, without a structure where all voices could be heard. So, Cliva
+            set to work to build a network, a governance, one where the 
+            suffering, the poor, and the innocent would have a place. Where no
+            one would be ignored. Where greed would be replaced by generosity
+            where cruelty would give way to compassion
         </div>
        </div>
     </div>
 
       <div className=' flex justify-center'>
-        <img src={RedShape} alt="" className=" w-[50%] absolute bottom-0 " style={{marginBottom:"2%"}}/>
-        <div className=" absolute bottom-0 text-white  font-secondRegular text-center" style={{marginBottom:"7%"}} >
-        <p className=' font-primaryRegular text-xl  uppercase' style={{WebkitTextStroke: "1px #228EFD"}}>NEW ARENA IS AT THE FRONTIER  </p>
-        <div className=' text-center pt-4 text-lg'>
+        <img src={RedShape} alt="" className=" w-[60%] absolute bottom-0 md:mb-6 " />
+        <div className=" absolute bottom-0 text-white  font-secondRegular text-center" >
+        <p className=' font-primaryRegular md:text-sm  uppercase ' style={{WebkitTextStroke: "1px #228EFD"}}>NEW ARENA IS AT THE FRONTIER  </p>
+        <div className=' text-center pt-4  md:mb-9 lg:mb-14'>
             <p>As she gazed over the land she had planned to </p>
             <p>Reshape, Cliva smiled.</p>
         </div>
